f1708ef4946cc4bf2152d3c8bbe9758c202a3351
Prajwol Gyawali <prajwol.lucifer@gmail.com>
2020-07-03 13:40:56 +0545

    Redundant code removed.

--- ODIContracts/token.py
+++ ODIContracts/token.py
@@ -8,12 +8,4 @@ from .tokens.IRC2 import IRC2
 TAG = 'SampleToken'
 
 class SampleToken(IRC2Mintable):
-
-	def __init__(self, db: IconScoreDatabase) -> None:
-		super().__init__(db)
-
-	def on_install(self, _tokenName: str, _symbolName: str, _initialSupply: int, _decimals: int = 18) -> None:
-		super().on_install(_tokenName, _symbolName, _initialSupply, _decimals)
-
-	def on_update(self, _tokenName: str, _symbolName: str, _initialSupply: int, _decimals: int = 18,) -> None:
-		super().on_update(_tokenName, _symbolName, _initialSupply, _decimals)
+	pass
