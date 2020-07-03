c74a4980e63c7d3a83304dc0d341df8ac623ff6a
Newton Poudel <hinewton25@gmail.com>
2020-07-03 13:27:50 +0545

    Docstrings Added

--- ODIContracts/tokens/IRC2burnable.py
+++ ODIContracts/tokens/IRC2burnable.py
@@ -6,13 +6,26 @@ class IRC2Burnable(IRC2):
 
 	@external
 	def burn(self, _amount: int) -> None:
-		# _burn is from IRC2
+		'''
+		Destroys `_amount` number of tokens from the caller account.
+		Decreases the balance of that account and total supply.
+		See {IRC2-_burn}
+
+		:param _amount: Number of tokens to be destroyed.
+		'''
 		super()._burn(self.msg.sender, _amount)
 
 	@external
 	def burnFrom(self, _account: Address, _amount: int) -> None:
+		'''
+		Destroys `_amount` number of tokens from the caller account.
+		Decreases the balance of that account and total supply.
+		See {IRC2-_burn}
+
+		:param _account: The account at whhich token is to be destroyed.
+		:param _amount: Number of tokens to be destroyed at the `_account`.
+		'''
 		self._decreasedAllowance = allowance(_account, SafeMath.sub(self.msg.value, _amount))
 
-		 #these are from IRC2
 		super()._approve(_account, self.msg.sender, self._decreasedAllowance)
 		super()._burn(_account, _amount)
--- ODIContracts/tokens/IRC2mintable.py
+++ ODIContracts/tokens/IRC2mintable.py
@@ -6,13 +6,26 @@ class IRC2Mintable(IRC2):
 
 	@external
 	def mint(self, _amount: int) -> None:
-		# _burn is from IRC2
+		'''
+		Creates `_amount` number of tokens, and assigns to caller account.
+		Increases the balance of that account and total supply.
+		See {IRC2-_mint}
+
+		:param _amount: Number of tokens to be created at the account.
+		'''
 		super()._mint(self.msg.sender, _amount)
 
 	@external
 	def mintTo(self, _account: Address, _amount: int) -> None:
+		'''
+		Creates `_amount` number of tokens, and assigns to `_account`.
+		Increases the balance of that account and total supply.
+		See {IRC2-_mint}
+
+		:param _account: The account at whhich token is to be created.
+		:param _amount: Number of tokens to be created at the account.
+		'''
 		self._increasedAllowance = self._allowance(_account, SafeMath.add(self.msg.value, _amount))
 
-		 #these are from IRC2
 		super()._approve(_account, self.msg.sender, self._increasedAllowance)
 		super()._mint(_account, _amount)
