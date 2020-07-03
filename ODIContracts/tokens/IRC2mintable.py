07f72df60c7a4e3e14da642d27e85e1f5d439456
Prajwol Gyawali <prajwol.lucifer@gmail.com>
2020-07-03 12:42:38 +0545

    Removed redundant codes

--- ODIContracts/tokens/IRC2mintable.py
+++ ODIContracts/tokens/IRC2mintable.py
@@ -4,15 +4,6 @@ from ..math.SafeMath import SafeMath
 
 class IRC2Mintable(IRC2):
 
-	def __init__(self, db: IconScoreDatabase) -> None:
-		super().__init__(db)
-
-	def on_install(self, _tokenName:str, _symbolName:str, _initialSupply:int, _decimals:int = 18) -> None:
-		super().on_install(_tokenName, _symbolName, _initialSupply, _decimals)
-
-	def on_update(self, _tokenName:str, _symbolName:str, _initialSupply:int, _decimals:int = 18) -> None:
-		super().on_update(_tokenName, _symbolName, _initialSupply, _decimals)
-
 	@external
 	def mint(self, _amount: int) -> None:
 		# _burn is from IRC2
