bf6760cec2890d2de84c2898fcd910ee2b8d6423
lilixac <hinewton25@gmail.com>
2020-11-27 16:48:07 +0545

    Checks added in burnFrom method

--- ODIContracts/tokens/IRC2/IRC2burnable.py
+++ ODIContracts/tokens/IRC2/IRC2burnable.py
@@ -21,15 +21,21 @@ class IRC2Burnable(IRC2, Roles):
 	@external
 	def burnFrom(self, _account: Address, _amount: int) -> None:
 		'''
-		Destroys `_amount` number of tokens from the specified `_account` account.
+		Destroys `_amount` number of tokens from the specified `_account` account
+		If `self.msg.sender` is approved by `_acccount`.
 		Decreases the balance of that account and total supply.
 		See {IRC2-_burn}
 
 		:param _account: The account at which token is to be destroyed.
 		:param _amount: Number of tokens to be destroyed at the `_account`.
 		'''
+		if (self._allowance(_account, self.msg.sender) <= 0):
+			revert(f"{self.msg.sender} not approved by {_account}.")
 
-		# decreasedAllowance = self._allowance(_account, self.msg.sender) - _amount
+		if (self._allowance(_account, self.msg.sender) < _amount):
+			revert(f"Only {_amount} was approved to {self.msg.sender}.")
 
-		# super()._approve(_account, self.msg.sender, decreasedAllowance)
+		decreasedAllowance = self._allowance(_account, self.msg.sender) - _amount
+
+		super()._approve(_account, self.msg.sender, decreasedAllowance)
 		super()._burn(_account, _amount)
